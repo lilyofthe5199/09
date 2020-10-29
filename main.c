e045dbd91995f2834666d2aad7a2d6509ef2d724
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-29 16:23:11 +0900

    05

--- main.c
+++ main.c
@@ -2,27 +2,52 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
+void addMatrx(int A[][COLS], int B[][COLS], int C[][COLS]);
+void printMatrx(int A[][COLS]);
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int A[ROWS][COLS] ={ // 행렬 A 
+	{2, 3, 0},
+	{8, 9, 1},
+	{7, 0, 5}};
 	
-	for(i=0; i<SIZE; i++)
-	{
-		grade[i]=rand()%100+1;
-	}
+
+ 	int B[ROWS][COLS] ={ // 행렬 B 
+	{1, 0, 0},
+	{0, 1, 0},
+	{0, 0, 1}};
 	
-	for(i=0; i<SIZE; i++)
-	{
-		score[i]=grade[i];		// 배열 값 복사 
-	}
+	int C[ROWS][COLS];	// 덧셈 결과 행렬 
 	
-	for(i=0; i<SIZE; i++)
-	{
-		printf("score[%d]=%i(%i)\n", i, score[i], grade[i]);
-	}
+	addMatrx(A, B, C);
+	printMatrx(C);
 	
 	return 0;
 }
+
+	void addMatrx(int A[][COLS], int B[][COLS], int C[][COLS])
+	{
+		
+		int i, j;
+		for(i=0; i<3; i++)	//for문을 이용해서 계산
+		{
+			for(j=0; j<3; j++)
+			C[i][j]=A[i][j]+B[i][j];	//C=A+B 형태로 행렬 계산
+		}
+		
+	}
+	
+	void printMatrx(int A[][COLS])
+	{
+		
+		int i, j;
+		for(i=0; i<3; i++)	//for문을 이용해서 계산
+		{
+			for(j=0; j<3; j++)
+			printf("%d ", A[i][j]);
+			printf("\n");
+		}
+		
+	}
